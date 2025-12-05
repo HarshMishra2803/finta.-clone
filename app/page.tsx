8489bb52702d1a7bf617fda43c94725b437596e0
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-06 02:15:21 +0530

    _

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <div
       className="h-screen relative
-      bg-blue-50">
+      bg-blue-100">
         <div className="max-w-5xl mx-auto absolute inset-0  h-full w-full">
           <div className="absolute inset-y-0 left-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
           <div className="absolute inset-y-0 right-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
@@ -16,7 +16,7 @@ const page = () => {
         <Navbar />
         <Hero/>
       </Container>
-      <div className = "relative w-full ">f
+      <div className = "relative w-full ">
         <div className="h-px w-full absolute inset-x-0 bg-gradient-to-r from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
         <div className="max-w-6xl mx-auto p-4">
         <Image src="/hero-ui-v5.webp" alt="" width={1000} height={1000}
