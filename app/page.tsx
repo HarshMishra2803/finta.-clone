8cafac5115a0400ec368af6b4b0e7ac391ef3dd3
Harsh Mishra <mishraharsh.2803@gmail.com>
2025-12-06 19:45:28 +0530

    _

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <div
       className="h-screen relative
-      bg-blue-100">
+      bg-blue-50">
         <div className="max-w-5xl mx-auto absolute inset-0  h-full w-full">
           <div className="absolute inset-y-0 left-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
           <div className="absolute inset-y-0 right-0 h-full w-px bg-gradient-to-b from-neutral-300/50 via-neutral-200 to-transparent pointer-events-none"></div>
